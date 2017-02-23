f085ecff59bba367eb9544f870f595f7481b99e2
Anamika Sahay <sahay.anamika@gmail.com>
2017-02-22 20:19:53 -0800

    my first comit

---
+++ src/TestConfig.java
@@ -0,0 +1,25 @@
+
+
+public class TestConfig{
+
+
+	
+	public static String server="smtp.gmail.com";
+	public static String from = "sahay.anamika@gmail.com";
+	public static String password = "Omchild@123";
+	public static String[] to ={"chebadri@gmail.com"};
+	public static String subject = "Test Report";
+	
+	public static String messageBody ="TestMessage";
+	public static String attachmentPath=System.getProperty("user.dir")+"//Reports.zip";
+	public static String attachmentName="reports.zip";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
